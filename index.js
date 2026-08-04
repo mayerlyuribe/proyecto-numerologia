6eaad9b79648dc6a73951f670af14071cef82939
mayerlyuribe <mayerlyuribe25@gmail.com>
2026-08-04 16:07:00 -0500

    conexión a la base de datos MongoDB exitosa

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+
+const app = express();
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
